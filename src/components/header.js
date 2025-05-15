24f81a0031981fa447c560f6928aeff6d0737bc5
Phanuelle <phanmanuel9@gmail.com>
2025-05-15 11:22:25 -0500

    adding header and proper color scheme

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
 import './header.css';
+// import "../app/globals.css";
 
 const Header = () => {
     return (
         <div className="header">
-            <h1 className="logo">Home Page</h1>
+            <h1 className="logo">Subscription Tracker</h1>
             <div className="links">
                 <Link href="/">Home Page</Link>
                 <Link href="/subscription">Subscriptions</Link>
