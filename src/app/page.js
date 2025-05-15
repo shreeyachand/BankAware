7dbdaf50410f33dd6bf0f5d1d2bb66153e1781ee
Phanuelle <phanmanuel9@gmail.com>
2025-05-15 11:08:53 -0500

    fixing duplicate styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 import styles from "./globals.css";
 import Subscription from "@/components/Subscription";
 
