1db3003ef8fafa5a5ae49b6731ed919e702e335d
Shreeya Chand <sachand@usc.edu>
2025-05-15 16:51:08 -0400

    filled in subscription data for active & recommended subs

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -1,13 +1,19 @@
 'use client'; // Only needed if using client-side features like useState/useEffect
 
 import './Subscription.css'; // Import the CSS file for styling
-const Subscription = ({ name, type, image, cost}) => {
+const Subscription = ({ name, type, image, cost, active, savings}) => {
   return (
     <div className="subscription">
         <img src={image} alt={`${name} logo`} />
         <div className="sub-info">
             <p>{type}</p>
-            <p>${cost} monthly</p>
+            {active && 
+                <p>${cost} monthly</p>                
+            }
+            {!active && 
+                <p>Estimated savings: ${savings} monthly</p>                
+            }
+            
         </div>
     </div>
   );
