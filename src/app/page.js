48667a9acabf9c042f33de58de456b655c983403
Phanuelle <phanmanuel9@gmail.com>
2025-05-15 19:10:01 -0500

    adding headshot captions

--- src/app/page.js
+++ src/app/page.js
@@ -6,27 +6,34 @@ import { activeSubscriptions, recommendedSubscriptions } from "@/data/data";
 export default function Home() {
   return (
     <>
-      <h2>Welcome to BankAware, your personal subscription tracker and banking "know-it-all."</h2>
+      <h2>
+        Welcome to BankAware, your personal subscription tracker and banking
+        "know-it-all."
+      </h2>
       {/*Sample of using subscription component, will be filled with our data*/}
       <h3>Meet the Team!</h3>
       <div className="headshots-container">
-        <img
-          src="/Phanuelle_Manuel_square.jpg"
-          alt="Phanuelle"
-        />
-        <img
-          src="/shreeya-headshot.jpeg"
-          alt="Shreeya"
-        />
-        <img
-          src="/Joshua-Headshot.jpg"
-          alt="Joshua"
-        />
-        <img
-          src="/images/benjamin.jpeg"
-          alt="Bridget"
-        />
+        <div className="phanuelle">
+          <img src="/Phanuelle_Manuel_square.jpg" alt="Phanuelle Manuel" />
+          <h4>Phanuelle Manuel</h4>
+          <p>Freshman Computer Science major @ Uni. of Alabama</p>
         </div>
+        <div className="shreeya">
+          <img src="/shreeya-headshot.jpeg" alt="Shreeya Chand" />
+          <h4>Shreeya Chand</h4>
+          <p>Freshman _________ major ________</p>
+        </div>
+        <div>
+        <img src="/Joshua-Headshot.jpg" alt="Joshua Udo" />
+        <h4>Joshua Udo</h4>
+          <p>Freshman ________ major @ Grambling State University </p>
+          </div>
+        <img src="/images/benjamin.jpeg" alt="Bridget Jiang" />
+        <div>
+        <h4>Bridget Jiang</h4>
+          <p>Freshman _______ major @ MIT</p>
+          </div>
+      </div>
     </>
   );
 }
